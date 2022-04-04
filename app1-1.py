c6916f1f2c92909cf7eb9644c4c609618bf1b901
naderbouaziz2020 <60794997+naderbouaziz2020@users.noreply.github.com>
2022-04-04 17:29:47 +0200

    Add files via upload

--- app1-1.py
+++ app1-1.py
@@ -15,6 +15,7 @@ ChestPainType_d = {0:"ATA",1:"NAP", 2:"ASY"}
 RestingECG_d = {0:"Normal",1:"ST"}
 ExerciseAngina_d = {0:"N",1:"Y"}
 ST_Slope_d = {0:"Up",1:"Flat"}
+FastingBS_d = {0:"0",1:"1"}
 # o ile wcześniej kodowaliśmy nasze zmienne, to teraz wprowadzamy etykiety z ich nazewnictwem
 
 def main():
@@ -34,15 +35,17 @@ def main():
 		ChestPainType_radio = st.radio( "ChestPainType", list(ChestPainType_d.keys()), format_func=lambda x: ChestPainType_d[x])
 		RestingECG_radio = st.radio( "RestingECG", list(RestingECG_d.keys()), format_func=lambda x : RestingECG_d[x])
 		ExerciseAngina_radio = st.radio( "ExerciseAngina", list(ExerciseAngina_d.keys()), format_func=lambda x : ExerciseAngina_d[x])
-		ST_Slope_radio = st.radio( "ST_Slope", list(ST_Slope_d.keys()), index=2, format_func= lambda x: ST_Slope_d[x])
+		ST_Slope_radio = st.radio( "ST_Slope", list(ST_Slope_d.keys()), format_func= lambda x: ST_Slope_d[x])
+		FastingBS_radio = st.radio( "ST_Slope", list(FastingBS_d.keys()), format_func= lambda x: FastingBS_d[x])
 
 	with right:
 		age_slider = st.slider("Age", value=1, min_value=1, max_value=100)
 		RestingBP_slider = st.slider("RestingBP", min_value=0, max_value=200)
 		Cholesterol_slider = st.slider("Cholesterol", min_value=0, max_value=1000)
 		MaxHR_slider = st.slider("MaxHR", min_value=0, max_value=500, step=1)
+		Oldpeak_slider = st.slider("Oldpeak", min_value=0, max_value=20, step=1)
 
-	data = [[ChestPainType_radio, sex_radio,  age_slider, RestingECG_radio, ExerciseAngina_radio, ST_Slope_radio, RestingBP_slider, Cholesterol_slider, MaxHR_slider]]
+	base_data = [[age_slider, sex_radio, ChestPainType_radio, RestingBP_slider, Cholesterol_slider, FastingBS_radio, RestingECG_radio,  MaxHR_slider, ExerciseAngina_radio, Oldpeak_slider, ST_Slope_radio   ]]
 	HeartDisease = model.predict(base_data)
 	s_confidence = model.predict_proba(base_data)
 
@@ -52,4 +55,4 @@ def main():
 		st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][HeartDisease][0] * 100))
 
 if __name__ == "__main__":
-    main()
+        main()
